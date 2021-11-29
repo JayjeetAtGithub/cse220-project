322a97533f92df829f47912d1ae6d2a77c0de621
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-11-29 11:11:33 -0800

    Initial Commit

---
+++ analyze_per.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    result = {
+        "stage": [],
+        "time (s)": []
+    }
+    numbers = [1, 2, 3, 4, 5]
+    for num in numbers:
+        with open(f"result_openroad.log.{num}") as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip().split(":")
+                result["stage"].append(line[0])
+                result["time (s)"].append(float(line[1]))
+
+    df = pd.DataFrame(result).groupby("stage").mean() 
+    dd = df["time (s)"].to_dict()
+
+    sum = 0
+    for k, v in dd.items():
+        sum += float(v)
+    
+    for k, v in dd.items():
+        val = (float(v) / sum) * 100
+        print(k, ": ",  val)
+
---
+++ analyze_stages.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    result = {
+        "stage": [],
+        "time (s)": []
+    }
+    numbers = [1, 2, 3, 4, 5]
+    for num in numbers:
+        with open(f"result_openroad.log.{num}") as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip().split(":")
+                result["stage"].append(line[0])
+                result["time (s)"].append(float(line[1]))
+
+    df = pd.DataFrame(result)
+    print(df)
+    sns_plot = sns.barplot(x="stage", y="time (s)", ci="sd", data=df)
+    fig = sns_plot.get_figure()
+    plt.title("Time spent in different design flow stages")
+    fig.savefig("plot_1_timing.png")
